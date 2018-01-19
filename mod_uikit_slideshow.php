a969ca704abc4783fd91a075291569c75e1554e0
# <#>
2018-01-19 15:08:11 +0100

    first commit

---
+++ mod_uikit_slideshow.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Uikit 2 Slideshow module
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_uikit_slideshow
+ *
+ * @author      Elvis Sedić <elvis@spletodrom.si>
+ * @copyright   Copyright (C) 2018 Elvis Sedić. All rights reserved.
+ * @license     GNU General Public License version 2 or later;
+ * @link        https://github.com/esedic/mod_uikit_slideshow
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die;
+
+// todo:
+// add optional assets loading
+// add uikit slideshow params
+// other
+
+// load jQuery
+JHtml::_('jquery.framework');
+
+// load Uikit assets
+$doc = JFactory::getDocument();
+$doc->addStyleSheet(JUri::root() . 'media/mod_uikit_slideshow/css/uikit.css');
+$doc->addStyleSheet(JUri::root() . 'media/mod_uikit_slideshow/css/slideshow.css');
+$doc->addStyleSheet(JUri::root() . 'media/mod_uikit_slideshow/css/slidenav.css');
+$doc->addStyleSheet(JUri::root() . 'media/mod_uikit_slideshow/css/dotnav.css');
+$doc->addScript(JUri::root() . 'media/mod_uikit_slideshow/js/uikit.min.js');
+$doc->addScript(JUri::root() . 'media/mod_uikit_slideshow/js/slideshow.min.js');
+
+// todo: advanced animation
+// if($animation == 1) {
+// 	$doc->addScript(JUri::root() . 'media/mod_uikit_slideshow/js/slideshow-fx.min.js');
+// }
+
+/* Get module id */
+$module_id = $module->id;
+
+/* Get module parameters */
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+// Set autoplay
+$autoplay = (int) $params->get('autoplay', 1);
+$autoplay = $params->get('autoplay') !== 1 ? 'autoplay:true' : '';
+
+// Set autoplay interval
+$interval = (int) $params->get('interval', 3000);
+$interval = $params->get('interval') !== 3000 ? ', autoplayInterval:' . $interval : '';
+
+// Set animation
+$animation = $params->get('animation', 'fade');
+if($animation == "scroll") {
+	$animation =  ", animation:'scroll'";
+}
+else if($animation == "scale") {
+	$animation =  ", animation:'scale'";
+}
+else if($animation == "swipe") {
+	$animation =  ", animation:'swipe'";
+}
+else {
+    $animation = "";
+}
+
+// Set pause on hover
+$pause = (int) $params->get('pause', 1);
+$pause = $params->get('pause') !== 1 ? ', pauseOnHover:true' : '';
+
+// Set dotnav & slidenav
+$dotnav = (int) $params->get('dotnav', 1);
+$slidenav = (int) $params->get('slidenav', 1);
+
+// Get images
+$images = $params->get('images');
+
+require JModuleHelper::getLayoutPath('mod_uikit_slideshow', $params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Uikit 2 Slideshow Module
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_uikit_slideshow
+ *
+ * @author      Elvis Sedić <elvis@spletodrom.si>
+ * @copyright   Copyright (C) 2018 Elvis Sedić. All rights reserved.
+ * @license     GNU General Public License version 2 or later;
+ * @link        https://github.com/esedic/mod_uikit_slideshow
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die;
+
+// todo:
+// Transitions
+// Ken Burns effect
+// Fullscreen slideshow
+// Video
+// Multilingual
+// jQuery check
+
+if ($images !== null) :
+?>
+
+<div id="UikitSlideshow-<?php echo $module_id; ?>">
+    <div class="uk-slidenav-position" data-uk-slideshow="{<?php echo $autoplay.$interval.$animation.$pause;?>}">
+
+        <!-- Container -->
+        <ul class="uk-slideshow">
+        <?php foreach ($images as $k => $image) : ?>
+                <!--
+                <?php // $class = $k == 0 ? ' class="uk-active"' : '';?>
+                <li<?php // echo $class;?>>
+                -->
+            <li>
+                <img src="<?php echo JUri::root() . $image->main_image; ?>" alt="<?php echo $image->title; ?>">
+                <?php if ($image->description || ($image->link) !== '') : ?>
+                    <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade"><?php echo $image->description; ?></div>
+                <?php if ($image->link !== '') : ?>
+                    <a class="uk-position-cover" href="<?php echo $image->link; ?>"<?php echo $image->target==1 ? ' target="_blank"' : ''; ?>></a>
+                <?php endif; ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+
+        <!-- Slidenav -->
+        <?php if ($slidenav === 1) : ?>
+        <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
+        <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slideshow-item="next"></a>
+        <?php endif; ?>
+
+        <!-- Dotnav -->
+        <?php if ($dotnav === 1) : ?>
+        <ul class="uk-dotnav uk-dotnav-contrast uk-position-bottom uk-flex-center">
+            <?php foreach ($images as $k => $image) : ?>
+            <li data-uk-slideshow-item="<?php echo $k; ?>"><a href=""></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
+
+    </div>
+</div>
+
+
+    <!-- Alert if there are no images -->
+<?php else : ?>
+<div class="uk-alert uk-alert-danger">
+    <h4>Error!</h4>
+    <p>There are no images.</p>
+</div>
+<?php endif; ?>
